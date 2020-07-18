b19d79a7749969d2cb840e30e145c764cfea2903
Mauricio Gutierrez <mauriciogtrz@g.ucla.edu>
2020-07-18 16:14:14 -0700

    Added data in seeds.sql and configured database values in schema.sql

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("Spicy Chonken Delooks", 0);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Pork Borrger ", 0);
+INSERT INTO burgers (burger_name, devoured) VALUES ("No Burger... just bun", 0);
